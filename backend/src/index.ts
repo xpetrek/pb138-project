4e905d25bd4376716fba32797fc159401723f5a1
Martin Kačenga <456274@mail.muni.cz>
2022-05-23 16:46:03 +0200

    fixed router and signup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import loginRoute from './routes/Login';
+import signupRoute from './routes/SignUp'
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/login', loginRoute);
+app.use(loginRoute);
+app.use(signupRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello from A!')
+})
 
 app.listen(PORT, () => {
   console.log('Backend server is running');
 });
+
+export default app;
--- backend/src/routes/SignUp.ts
+++ backend/src/routes/SignUp.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import jsonwebtoken from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt'
 
@@ -9,9 +8,13 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
     const { name, email, password } = req.body;
+    console.log(name, email, password);
 
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    console.log(passwordHash);
+
 
     const createdUser = await prisma.user.create({
         data: {
@@ -21,13 +24,14 @@ router.post("/signup", async (req, res) => {
         }
     });
 
+    console.log(createdUser);
 
     if (createdUser === null) {
         return res
           .status(401)
           .json({ message: "The sign-up has failed!" });
     }
-    return res.status(200);
+    res.status(200).send("Successfully created an user!");
 });
 
 export default router;
